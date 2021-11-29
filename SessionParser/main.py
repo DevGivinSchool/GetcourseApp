94c14dcaa5542f453a07ec2dea8af3ff9ac56b69
Бобровский Дмитрий <ministrbob777@gmail.com>
2021-11-29 22:25:31 +0300

    add error_handling and get_env

--- SessionParser/main.py
+++ SessionParser/main.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
+
 import logging
+import os
+import traceback
+import sys
 
 
 def config_logging():
@@ -10,6 +15,32 @@ def config_logging():
     custom_logger.addHandler(handler)
 
 
+def error_handling(message, do_exit=False):
+    error_text = f"ERROR: {message}\n{traceback.format_exc()}"
+    logging.error(error_text)
+    if do_exit:
+        sys.exit(f"Программа завершилась ошибкой\n{error_text}")
+
+
+def get_env():
+    logging.info('Get environment variables')
+    GCL: str | None = os.getenv('gcl')
+    GCP: str | None = os.getenv('gcp')
+    try:
+        if GCL is None:
+            raise EnvironmentError("Не определена переменная GCL")
+        if GPL is None:
+            raise EnvironmentError("Не определена переменная GPL")
+    except EnvironmentError as err:
+        error_handling(str(err), do_exit=True)
+    logging.debug(GCL)
+    logging.debug(GCP)
+
+
 if __name__ == '__main__':
+    # Конфигурируем формат логирования
     config_logging()
     logging.info('SessionParser start')
+    # Получаем значения переменных с приватными данными
+    get_env()
+    # TODO Инициализация БД
