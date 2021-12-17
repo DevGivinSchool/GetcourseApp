7cca7e3418de6799ab7bfc5b1e739e38705b6c08
Бобровский Дмитрий <ministrbob777@gmail.com>
2021-12-17 14:10:23 +0300

    CREATE TABLE sessions

--- SessionParser/sql.py
+++ SessionParser/sql.py
@@ -2,35 +2,8 @@
 sql_ct_last_date = "CREATE TABLE last_date (value date)"
 
 # create table sessions
-'''
-ID визита
-Начало визита
-IP
-Тип трафика
-Канал
-Глубина просмотра
-Номер визита
-ID посетителя
-Устройство
-Страница входа
-Адрес входа
-Referrer входа
-Пользователь
-Заказ
-Группа каналов
-Группа расходов
-Группа расходов
-Кампания
-UTM-medium
-UTM-source
-Ключевое слово
-UTM-Content
-Реферер - значимый домен
-Реферер - домен
-Реферер
-Страница старта
-Глубина просмотра
-Есть заказ
-Есть заказ
-'''
-sql_ct_sessions = """CREATE TABLE sessions (name, value)"""
+sql_ct_sessions = """CREATE TABLE sessions (visit_id,start_of_visit,ip,traffic_type,channel,depth_of_view,
+visit_number,visitor_id,device,login_page,entrance_address,login_referrer,user,order,channel_group,expense_group_1,
+expense_group_2,campaign,utm_medium,utm_source,keyword,utm_content,referrer_significant_domain,referrer_domain,
+referrer,start_page,depth_of_view,there_is_an_order_1,there_is_an_order_2,country,region,city,ip2,user_id,user_url,
+user_email,user_telegram,user_country,user_city) """
